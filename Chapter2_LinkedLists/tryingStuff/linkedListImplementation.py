9c682195acb0bf7b1f181d5fb4482e6f30ed0649
Eduard <jeskoed@gmail.com>
2021-01-07 20:04:01 +0000

    newCode

--- Chapter2_LinkedLists/tryingStuff/linkedListImplementation.py
+++ Chapter2_LinkedLists/tryingStuff/linkedListImplementation.py
@@ -1,2 +1,8 @@
+# source https://medium.com/@kojinoshiba/data-structures-in-python-series-1-linked-lists-d9f848537b4d
+
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.next = None # the pointer initially points to nothing
+
 print('test')
-# test
