06c2c4388b43acc1b2add10941e0f9d75686c16f
Eduard <jeskoed@gmail.com>
2021-01-18 22:39:02 +0000

    a

--- Chapter2_LinkedLists/questions/2_1_remove_dups.py
+++ Chapter2_LinkedLists/questions/2_1_remove_dups.py
@@ -1,5 +1,15 @@
 from linkedListOwn import Node, LinkedList
 
+def remove_dups_with_array(linkedList):
+    if linkedList.head == None:
+        return
+    itr = linkedList.head
+    uniqueValues = []
+    while itr:
+        if (itr.data in uniqueValues):
+            linkedList.
+        itr = itr.next
+
 if __name__ == "__main__":    
     ll = LinkedList()
     ll.insert_values(['A','B','C','D','B','B','A'])
