5fb1743f9e931a38d9b29360a873a77f0701c0b4
Eduard <jeskoed@gmail.com>
2021-01-18 21:11:17 +0000

    com

--- Chapter2_LinkedLists/questions/linkedListOwn.py
+++ Chapter2_LinkedLists/questions/linkedListOwn.py
@@ -17,13 +17,34 @@ class LinkedList:
             llstr += str(itr.data) + '=>'
             itr = itr.next
         print(llstr)
-        
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(data, None)
+    
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+    
+    def getLength(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+        return count
+
+
 
 ll = LinkedList()
-ll.addNodeAtBeginning('ed')
-ll.addNodeAtBeginning('ped')
+ll.insert_values(['red', 'bed', 'ced'])
 ll.printNodes()
-
 
 
 
