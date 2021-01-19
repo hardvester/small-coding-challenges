fd9bc5bda7c09b58d26d7bb41d06ab20dfb1a3b4
Eduard <jeskoed@gmail.com>
2021-01-19 21:09:26 +0000

    comm

--- Chapter2_LinkedLists/questions/2_1_remove_dups.py
+++ Chapter2_LinkedLists/questions/2_1_remove_dups.py
@@ -4,13 +4,39 @@ def remove_dups_with_array(linkedList):
     if linkedList.head == None:
         return
     itr = linkedList.head
-    uniqueValues = []
+    uniqueValues = [itr.data]
+    while itr.next:
+        if (itr.next.data in uniqueValues):
+            # I have done the movement here already
+            itr.next = itr.next.next
+        else:
+            uniqueValues.append(itr.data)
+            itr = itr.next
+    return linkedList
+
+def remove_dups_without_buffer(linkedList):
+    if linkedList.head == None:
+        return
+    itr = linkedList.head
     while itr:
-        if (itr.data in uniqueValues):
-            linkedList.
+        followingNode = itr.next
+        while followingNode:
+            print(followingNode.data)
+            if itr.data == followingNode.data:
+                itr.next = followingNode.next.next
+            else:
+                followingNode = followingNode.next
         itr = itr.next
+    return linkedList
+
+
+    
 
 if __name__ == "__main__":    
     ll = LinkedList()
-    ll.insert_values(['A','B','C','D','B','B','A'])
+    ll.insert_values(['A','B','A','C','D','B','B','A'])
     ll.printNodes()
+    ll = remove_dups_without_buffer(ll)
+    ll.printNodes()
+    test = {}
+    
