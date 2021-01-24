11d27350a5d52cf9588ed34ee582ed551a1606cb
Eduard <jeskoed@gmail.com>
2021-01-24 19:49:26 +0000

    works!!

--- Chapter2_LinkedLists/questions/2_2_kth_to_last.py
+++ Chapter2_LinkedLists/questions/2_2_kth_to_last.py
@@ -2,18 +2,22 @@ from linkedListOwn import Node, LinkedList
 
 # for us k = 1 is the last node, k = 0 is undefined
 def kthToLast(LinkedList, k):
+    
     p1 = LinkedList.head
     p2 = LinkedList.head
 
-    for i in range(0, k-1):
+
+    for _ in range(0, k-1):
         p1 = p1.next
         if p1.next == None:
             return # I should return something that range k is too big
 
-    while p1:
+    while p1.next:
         p1 = p1.next
         p2 = p2.next
-    
+
+    LinkedList.head = p2
+    return LinkedList
 
 
 
@@ -22,4 +26,4 @@ if __name__ == "__main__":
     ll = LinkedList()
     ll.insert_values(['A','B','A','s',2,333,23])
     ll.printNodes()
-    kthToLast(ll, 4).printNodes()
+    kthToLast(ll, 3).printNodes()
