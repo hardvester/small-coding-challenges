c72b79af91d8234dbdae2e774e7e35b84b7ea6e6
Eduard <jeskoed@gmail.com>
2021-01-25 20:55:42 +0000

    a

--- Chapter2_LinkedLists/questions/2_3_delete_middle_node.py
+++ Chapter2_LinkedLists/questions/2_3_delete_middle_node.py
@@ -1,7 +1,20 @@
 from linkedListOwn import Node, LinkedList
 
+# this is for the case when we have access to every node
+def delete_middle_node(ll, node):
+    itr = ll.head
 
+    while itr.next:
+        if itr.next.data == node.data:      
+            itr.next = itr.next.next   
+            return ll     
+        itr = itr.next
+    
+    
 if __name__ == "__main__":    
     ll = LinkedList()
-    ll.insert_values(['A','B','A','s',2,333,23])
+    ll.insert_values(['a','b','c','d', 'e', 'f'])
     ll.printNodes()
+    input_node = Node('c', 'd')
+    delete_middle_node(ll, input_node).printNodes()
+
