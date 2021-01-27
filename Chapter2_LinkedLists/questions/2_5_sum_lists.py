a64bea1a9edb5b099f2d4ac816196693de914216
Eduard <jeskoed@gmail.com>
2021-01-27 21:38:12 +0000

    c

---
+++ Chapter2_LinkedLists/questions/2_5_sum_lists.py
@@ -0,0 +1,12 @@
+from linkedListOwn import Node, LinkedList
+
+def sum_lists(ll1, ll2):
+    return
+
+if __name__ == "__main__":
+
+    ll1 = LinkedList()
+    ll2 = LinkedList()
+    ll1.insert_values([7,1,6])
+    ll2.insert_values([5,9,3])
+    ll1.printNodes()
