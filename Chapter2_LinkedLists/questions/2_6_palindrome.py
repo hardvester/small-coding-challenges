2a6514cd6d082f200e16e9a79fd6650a0eb4f340
Eduard <jeskoed@gmail.com>
2021-02-04 21:32:36 +0000

    c

--- Chapter2_LinkedLists/questions/2_6_palindrome.py
+++ Chapter2_LinkedLists/questions/2_6_palindrome.py
@@ -16,10 +16,23 @@ def reverseLL(ll):
 
 def isPalindrome(ll):
     reversedLL  = reverseLL(ll)
+ 
+    itr1 = ll.head
+    itr2 = reversedLL["ll"].head
+    for _ in range(reversedLL["ll_len"]//2):
+        print(itr1.data, itr2.data)
+        if itr1.data != itr2.data:
+            print('not a palindrome')
+            return
+        itr1 = itr1.next
+        itr2 = itr2.next
+    print('is a palindrome')
     return
+
+
 
 
 if __name__ == "__main__":
     ll1 = LinkedList()
-    ll1.insert_values([7,2])
-    print(reverseLL(ll1)["ll_len"])
+    ll1.insert_values(['g','e','g'])
+    isPalindrome(ll1)
