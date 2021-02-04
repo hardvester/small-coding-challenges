c6edddfc29b93ca3b18bc7a27b2fefcd1d5f3490
Eduard <jeskoed@gmail.com>
2021-02-04 21:12:53 +0000

    commit

--- Chapter2_LinkedLists/questions/2_6_palindrome.py
+++ Chapter2_LinkedLists/questions/2_6_palindrome.py
@@ -2,20 +2,20 @@ from linkedListOwn import Node, LinkedList
 
 def reverseLL(ll):
     itr = ll.head
-    last_element = itr.head
-    last_element.next = None
+    previous = Node(itr.data, None)
+    if itr.next is None:
+        return LinkedList(previous)
+       
     while itr.next:
-        node = itr.next
-    return
-
-
+        temp = Node(itr.next.data, previous)
+        previous = temp
+        itr = itr.next
+    
+    return LinkedList(temp)
 
 
 if __name__ == "__main__":
     ll1 = LinkedList()
-    ll2 = LinkedList()
-    ll1.insert_values([7,1,6])
-    ll2.insert_values([5,9,3])
-    ll1_copy = ll1
-    print(ll1.next)
-    print(ll1_copy.next)
+    ll1.insert_values([7])
+    ll1.printNodes()
+    reverseLL(ll1).printNodes()
