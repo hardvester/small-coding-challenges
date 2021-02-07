6eba27e803c8b6f241a63ed66af591120f8af26f
Eduard <jeskoed@gmail.com>
2021-02-07 22:59:24 +0000

    c

--- Chapter2_LinkedLists/questions/2_7_intersection.py
+++ Chapter2_LinkedLists/questions/2_7_intersection.py
@@ -1,7 +1,18 @@
 from linkedListOwn import Node, LinkedList
 
 def findIntersectNodes(ll1, ll2):
-    
+    ll1_length = ll1.getLength()
+    ll2_length = ll2.getLength()
+    length_diff = ll1_length - ll2_length
+
+    if length_diff>0:
+        starter_ll = ll1
+    elif length_diff<0:
+        starter_ll = ll2
+    else:
+        pass
+        # run in paraler
+
 
 
 
@@ -12,5 +23,7 @@ if __name__ == "__main__":
 
     ll1.printNodes()
     ll2.printNodes()
+    print(findIntersectNodes(ll1, ll2))
+
 
 
