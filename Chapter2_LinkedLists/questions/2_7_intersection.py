079f9d23efa08109c89c69c042471dac4be15f71
Eduard <jeskoed@gmail.com>
2021-02-07 22:51:06 +0000

    c

--- Chapter2_LinkedLists/questions/2_7_intersection.py
+++ Chapter2_LinkedLists/questions/2_7_intersection.py
@@ -1,1 +1,11 @@
 from linkedListOwn import Node, LinkedList
+
+
+
+
+if __name__ == "__main__":    
+    intersect = Node('x', Node('z', None))
+    ll1 = LinkedList(Node('p', intersect))
+    ll1.printNodes()
+
+
