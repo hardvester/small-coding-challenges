2718366806033ee0b9d6084bc5ed98da2be7147d
Eduard <jeskoed@gmail.com>
2021-02-15 21:37:30 +0000

    c

--- Chapter3_Stacks_and_Queues/3_1_three_in_one.py
+++ Chapter3_Stacks_and_Queues/3_1_three_in_one.py
@@ -10,23 +10,21 @@ class multiStack:
         self.array = [None] * self.number_of_stacks * self.stack_size
 
     def push(self, value, stack_number):
-        
-        
+        self.array[self.index(stack_number)] = value
+
     def index(self, stack_number):
-        # stack = 1
-        offset = (stack_number * self.stack_size) - 1
+        offset = (stack_number - 1) * self.stack_size
         index = 0
         while True:
             if index > self.stack_size-1:
-                raise EnvironmentError('Stack number ' + stack_number + ' is full')
+                raise IndexError('Stack number ' + stack_number + ' is full')
             elif self.array[index + offset] is None:
                 return index
-            index += 1
-        
-
-    
+            index += 1    
 
 if __name__ == "__main__":
     new_multi_stack = multiStack(5)
+    new_multi_stack.push(4, 1)
+    new_multi_stack.push('a', 2)
     print(new_multi_stack.array)
         
