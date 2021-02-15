4cf6ff7943cb4e0a24c08580917895d4c889f254
Eduard <jeskoed@gmail.com>
2021-02-15 21:23:05 +0000

    c

--- Chapter3_Stacks_and_Queues/3_1_three_in_one.py
+++ Chapter3_Stacks_and_Queues/3_1_three_in_one.py
@@ -2,3 +2,31 @@
 # we implement the easier approach, i.e. approach where we pre allocate a fixed ammount of memory for each stack
 
 
+class multiStack:
+
+    def __init__(self, stack_size):
+        self.number_of_stacks = 3
+        self.stack_size = stack_size
+        self.array = [None] * self.number_of_stacks * self.stack_size
+
+    def push(self, value, stack_number):
+        
+        
+    def index(self, stack_number):
+        # stack = 1
+        offset = (stack_number * self.stack_size) - 1
+        index = 0
+        while True:
+            if index > self.stack_size-1:
+                raise EnvironmentError('Stack number ' + stack_number + ' is full')
+            elif self.array[index + offset] is None:
+                return index
+            index += 1
+        
+
+    
+
+if __name__ == "__main__":
+    new_multi_stack = multiStack(5)
+    print(new_multi_stack.array)
+        
