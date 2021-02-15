81542eacd686515ba848331dc25e406935325e77
Eduard <jeskoed@gmail.com>
2021-02-15 20:47:22 +0000

    c

--- Chapter3_Stacks_and_Queues/3_1_three_in_one.py
+++ Chapter3_Stacks_and_Queues/3_1_three_in_one.py
@@ -1,2 +1,4 @@
 # we will have to implement for this methods that are used for a stack: peep(), pop(), push(), isEmpty()
+# we implement the easier approach, i.e. approach where we pre allocate a fixed ammount of memory for each stack
+
 
