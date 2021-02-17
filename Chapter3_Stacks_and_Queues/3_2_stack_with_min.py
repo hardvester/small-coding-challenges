d21e5899c24ceeec9f0a48289886994b5b51b9b4
Eduard <jeskoed@gmail.com>
2021-02-17 21:15:07 +0000

    3_2_super_quick_finish

---
+++ Chapter3_Stacks_and_Queues/3_2_stack_with_min.py
@@ -0,0 +1,51 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+        self.min = []
+
+    def push(self, data):
+        print(self.min)
+        if not len(self.min):
+            self.min.append(data)
+        elif data <= self.min[-1]: 
+            self.min.append(data) 
+        self.items.append(data)
+    
+    def pop(self):
+        if self.items == []:
+            raise Exception('Stack empty')
+        if self.peep() == self.min[-1]:
+            self.min.pop()
+        self.items.pop()
+    
+    def isEmpty(self):
+        return self.items == []
+
+    def peep(self):
+        if self.isEmpty():
+            raise Exception('Stack is empty')
+        return self.items[len(self.items) - 1]
+
+    def get_min(self):
+        if self.isEmpty:
+            raise Exception('Stack is empty')
+        return self.min[-1]
+
+
+if __name__ == "__main__":
+    stack = Stack()
+    stack.push(5)
+    stack.push(6)
+    stack.push(4)
+    stack.push(4)
+    stack.push(5)
+    stack.pop()
+    stack.pop()
+    stack.pop()
+    stack.pop()
+    stack.pop()
+    
+
+    print(stack.get_min())
+
+
