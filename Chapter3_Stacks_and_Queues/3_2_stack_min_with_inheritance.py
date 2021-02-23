be8efa5ea8ee6d8f116d34387b7bcd563f0e5d6c
Eduard <jeskoed@gmail.com>
2021-02-23 22:37:11 +0000

    m

--- Chapter3_Stacks_and_Queues/3_2_stack_min_with_inheritance.py
+++ Chapter3_Stacks_and_Queues/3_2_stack_min_with_inheritance.py
@@ -3,11 +3,7 @@ from stack_implementation_with_list import Stack
 class StackMin(Stack):
     def __init__(self):
         self.min = []
-        Stack.__init__(self)
-
-
-    
-    
+        Stack.__init__(self)    
 
 if __name__ == "__main__":
     stack = StackMin()
