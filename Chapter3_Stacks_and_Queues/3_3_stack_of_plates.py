7660b1bd859863739a78659a21cff3acd5018359
Eduard <jeskoed@gmail.com>
2021-02-27 21:46:57 +0000

    c

--- Chapter3_Stacks_and_Queues/3_3_stack_of_plates.py
+++ Chapter3_Stacks_and_Queues/3_3_stack_of_plates.py
@@ -1,12 +1,15 @@
-class Stack:
+from stack_implementation_with_list import Stack
+
+class LimitedStack:
     def __init__(self, capacity=10):    
-        self.stack = []
         self.capacity = capacity
         self.counter = 0
-        self.stack_number = 0
+        self.stack_array = []
 
     def push(self, data):
         if self.counter > self.capacity:
             # I would need to create a new empty stack
+
+
 
 
