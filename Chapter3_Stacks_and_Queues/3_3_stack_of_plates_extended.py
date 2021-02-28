ec2939f206696795ba44d7ae5623eb67948a858d
Eduard <jeskoed@gmail.com>
2021-02-28 14:29:55 +0000

    c

---
+++ Chapter3_Stacks_and_Queues/3_3_stack_of_plates_extended.py
@@ -0,0 +1,22 @@
+# in addtion to stack of plates basic we implement the popAt function which
+# performs the pop operation on a specific sub stack
+
+from stack_implementation_with_list import Stack
+from stack import Limit
+
+class LimitedStackWithIndexPop():
+    
+        
+
+if __name__ == "__main__":
+    limited_stack = LimitedStack()
+    limited_stack.push(1)
+    limited_stack.push(2)
+    limited_stack.pop()
+    print(limited_stack.showStacks())
+    print(limited_stack.counter)
+    print(limited_stack.isEmpty())
+    print(limited_stack.peep())
+
+
+
