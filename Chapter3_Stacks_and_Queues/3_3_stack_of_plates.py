6bd18e1a0263226b9bc5a251f2778f27229e5554
Eduard <jeskoed@gmail.com>
2021-02-28 14:06:47 +0000

    c

--- Chapter3_Stacks_and_Queues/3_3_stack_of_plates.py
+++ Chapter3_Stacks_and_Queues/3_3_stack_of_plates.py
@@ -1,23 +1,52 @@
 from stack_implementation_with_list import Stack
 
 class LimitedStack:
-    def __init__(self, capacity=10):    
+    def __init__(self, capacity=2):    
         self.capacity = capacity
         self.counter = 0
         self.stack_array = []
 
-    def LimitedStackPush(self, data):
+    def push(self, data):
         if len(self.stack_array) == 0 or self.counter == self.capacity:
             new_stack = Stack()
             new_stack.push(data)
             self.stack_array.append(new_stack)
+            self.counter = 1
+            print('push 1')
         else:
-            stack = self.stack_array[-1]
+            stack = self.getLastStack()
             stack.push(data)
+            self.counter += 1
+            print('push 2')
+    def pop(self):
+        if self.counter == 0 and len(self.stack_array) == 1:
+            raise Exception('Stack empty')
+        elif self.counter == 0:
+            # this is the built in pop method used for the array
+            self.stack_array.pop()
+            stack = self.getLastStack()
+            stack.pop()
+            self.counter = self.capacity - 1
+        elif self.counter > 0:
+            print('pop')
+            stack = self.getLastStack()
+            stack.pop()
+            self.counter -= 1
 
-    def LimitedStackPop(self):
+    def getLastStack(self):
+        return self.stack_array[-1]
+
+    def showStacks(self):
+        return self.stack_array
         
 
+if __name__ == "__main__":
+    limited_stack = LimitedStack()
+    limited_stack.push(1)
+    limited_stack.push(2)
+    limited_stack.pop()
+    print(limited_stack.showStacks())
+    print(limited_stack.counter)
 
 
 
