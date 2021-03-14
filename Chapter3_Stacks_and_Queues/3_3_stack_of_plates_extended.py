c6e09d3a41cdbc59dbcb190923dfaca7dfd0de5a
hardvester <jeskoed@gmail.com>
2021-03-14 22:11:17 +0100

    com

--- Chapter3_Stacks_and_Queues/3_3_stack_of_plates_extended.py
+++ Chapter3_Stacks_and_Queues/3_3_stack_of_plates_extended.py
@@ -30,10 +30,57 @@ class Stack:
             raise Exception('Stack empty')
         return self.items[len(self.items) - 1]
 
+class LimitedStack:
+    def __init__(self, capacity=2):    
+        self.capacity = capacity
+        self.counter = 0
+        self.stack_array = []
+
+    def push(self, data):
+        if len(self.stack_array) == 0 or self.counter == self.capacity:
+            new_stack = Stack()
+            new_stack.push(data)
+            self.stack_array.append(new_stack)
+            self.counter = 1
+        else:
+            stack = self.getLastStack()
+            stack.push(data)
+            self.counter += 1
+    
+    def pop(self):
+        # improving by destructing the last stack?
+        if self.counter == 0 and len(self.stack_array) == 1:
+            raise Exception('Stack empty')
+        elif self.counter == 0:
+            self.stack_array.pop()
+            stack = self.getLastStack()
+            stack.pop()
+            self.counter = self.capacity - 1
+        else:
+            stack = self.getLastStack()
+            stack.pop()
+            self.counter -= 1
+
+    def popAt(self, stackID):
+        pass
+    
+    def isEmpty(self):
+        return self.stack_array[0].isEmpty()
+
+    def peep(self):
+        return self.getLastStack().peep()
+
+    def getLastStack(self):
+        return self.stack_array[-1]
+
+    def showStacks(self):
+        return self.stack_array
+
 if __name__ == "__main__":
-    stack = Stack()
+    stack = LimitedStack()
     stack.push(2)
     stack.push(3)
     stack.push(4)
-    print(stack.bottom)
+    
+    print(stack.showStacks())
     
