ba7449d9ed3545d0c6cd8a0984d86dbb3721b12d
Eduard <jeskoed@gmail.com>
2021-03-29 20:17:41 +0000

    c

--- Chapter3_Stacks_and_Queues/3_3_stack_of_plates_extended.py
+++ Chapter3_Stacks_and_Queues/3_3_stack_of_plates_extended.py
@@ -3,6 +3,7 @@
 
 class Stack:
     def __init__(self):
+        # we will be tracking both the bottom and top of the stack
         self.items = []
         self.top = None
         self.bottom = None
@@ -60,30 +61,26 @@ class LimitedStack:
             stack = self.getLastStack()
             stack.pop()
             self.counter -= 1
-
-    def popAt(self, stackId):
-        # if last stack ==> simply pop
-        # else move around stack values
-        #if stackId == len(self.stack_array)-1:
-        #    self.pop()
-        #else:
-        return
+        
 
     def pop_at(self, index):
         return self.left_shift(index, True)
     
     def left_shift(self, index, remove_top):
-        stack = self.stacks[index] # fetching the stack with the selected index
-        removed_item = stack.pop() if remove_top else stack.remove_bottom() # normally this will always remove the top of the stack
-        if stack.is_empty():
-            del self.stacks[index] # destructs the stack
-        elif len(self.stacks) > index + 1:
+        stack = self.stack_array[index] # fetching the stack with the selected index
+        removed_item = stack.pop() if remove_top else stack.remove_bottom() # normally this will always remove the top
+                                                                            # of the stack, but we can remove the 
+        if stack.isEmpty():
+            del self.stack_array[index] # destructs the stack
+        elif len(self.stack_array) > index + 1: # if selected stack index is not the last stack
             v = self.left_shift(index + 1, False) # recursive function that deals with the stack
             stack.push(v)
         return removed_item
-    
-    
-            
+
+    # let's try to implement the popAt fucntion only for the last stack
+    # the pop function return what is remaining
+
+
     def isEmpty(self):
         return self.stack_array[0].isEmpty()
 
@@ -102,5 +99,5 @@ if __name__ == "__main__":
     stack.push(3)
     stack.push(4)
     
-    print(stack.showStacks())
+    print(stack.stack_array[1].top)
     
