275c0427e65e663a572f31326e0c30e18bc18be5
Eduard <jeskoed@gmail.com>
2021-04-05 17:01:32 +0000

    finished stack queue

--- Chapter3_Stacks_and_Queues/3_4_queue_via_stacks.py
+++ Chapter3_Stacks_and_Queues/3_4_queue_via_stacks.py
@@ -10,45 +10,37 @@ class MyQueue:
         return self.active_stack.isEmpty()
 
     def add(self, val):
-        if (self.stack_newest == self.active_stack):   
-            self.stack_newest.push(val)
-        else:
-            pass
-            # copy_stack
-    
-    def remove(self):
         if self.active_stack == self.stack_oldest:
-            if self.active_stack.isEmpty():
-                raise Exception('Stack is empty')
-            else:
-                self.active_stack.pop()
-        else:
-            pass
-            # copy_stack
-
-        while True:
-            temp = self.stack_newest.peep()
-            self.stack_newest.pop()
-
+            self.copy_stack(self.stack_oldest, self.active_stack)   
+        self.active_stack.push(val)    
+        
+    def remove(self):
+        if self.active_stack == self.stack_newest:
+            self.copy_stack(self.stack_newest, self.stack_oldest)
+        self.active_stack.pop()
     
     def peek(self):
         if self.active_stack == self.stack_newest:
             self.copy_stack(self.stack_newest, self.stack_oldest)
-        self.active_stack.peep()    
+        return self.active_stack.peep()    
 
     def copy_stack(self, original_stack, new_stack):
         while True:
-            new_stack.push(original_stack.peep()) 
-            original_stack.pop()
             if original_stack.isEmpty():
                 self.active_stack = new_stack
                 break      
+            else:
+                new_stack.push(original_stack.peep())
+                original_stack.pop()
 
 if __name__ == "__main__":
     test = MyQueue()
     test.add(1)
     test.add(2)
-    print(test.stack_oldest.peep())
+    test.add(3)
+    test.remove()
+    test.remove()
+    test.remove()
     print(test.peek())
 
 
