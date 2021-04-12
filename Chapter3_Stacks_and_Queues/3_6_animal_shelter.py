2c64358d86c33c225993feadab31202b17e2e0ba
hardvester <jeskoed@gmail.com>
2021-04-12 20:09:23 +0200

    c

---
+++ Chapter3_Stacks_and_Queues/3_6_animal_shelter.py
@@ -0,0 +1,16 @@
+from queue_implementation_with_list import Queue
+
+class Shelter:
+    def __init__(self):
+        self.dogQueue = Queue()
+        self.catQueue = Queue()
+        self.order = 0
+    
+    def enqueue(self):
+
+
+
+
+
+if __name__ == "__main__":
+    pass
--- Chapter3_Stacks_and_Queues/sandbox.py
+++ Chapter3_Stacks_and_Queues/sandbox.py
@@ -1,4 +1,1 @@
-array = [1,2,3,4,5]
-x = array.pop()
-print(x)
-print(array)
+class 
