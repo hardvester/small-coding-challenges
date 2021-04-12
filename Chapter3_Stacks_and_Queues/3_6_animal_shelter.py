4d02d4cc290ec802b00cf72e8addfcab9bfe8ec7
hardvester <jeskoed@gmail.com>
2021-04-12 20:38:53 +0200

    advancment

--- Chapter3_Stacks_and_Queues/3_6_animal_shelter.py
+++ Chapter3_Stacks_and_Queues/3_6_animal_shelter.py
@@ -1,12 +1,30 @@
 from queue_implementation_with_list import Queue
 
-class Shelter:
+class Animal():
+    def __init__(self, type, order):
+        self.type = type
+        self.order = order
+
+class AnimalShelter:
     def __init__(self):
         self.dogQueue = Queue()
         self.catQueue = Queue()
         self.order = 0
     
-    def enqueue(self):
+    def enqueue(self, animal):
+        # I got stuck on how to add the ordering to the queue
+        if animal.type == 'dog':
+            self.dogQueue.add(Animal(animal, self.order))
+        elif animal.type == 'cat':
+            self.catQueue.add(Animal(animal, self.order))
+        self.order += 1
+
+    def dequeue(self):
+        
+
+    def getOldest(self):
+        
+        
 
 
 
