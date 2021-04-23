33b278ba67425490099a3994533d4bc2b228754a
hardvester <jeskoed@gmail.com>
2021-04-23 14:54:21 +0200

    c

--- small_coding_challenges/chapter4_graphs/sandbox.py
+++ small_coding_challenges/chapter4_graphs/sandbox.py
@@ -1,3 +1,27 @@
-from queue import Queue
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-print('test')
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        return
+        f = ListNode()
+        
+        while True:
+            while l2:
+                if l2.val < l1.val:
+                    f.val = l2.val
+                    l2 = l2.next
+                    f = f.next
+                break
+            
+            f.val = l1.val
+            l1 = l1.next
+            f = f.next
+                    
+                
+
+sol = Solution()
+sol.mergeTwoLists([1,2,4][1,3,4])
