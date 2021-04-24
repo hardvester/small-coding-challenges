12f2068ccadba727254daf7ec765468676b2525f
hardvester <jeskoed@gmail.com>
2021-04-24 22:30:30 +0200

    finished

--- small_coding_challenges/chapter4_graphs/4_2_minimal_tree.py
+++ small_coding_challenges/chapter4_graphs/4_2_minimal_tree.py
@@ -12,14 +12,18 @@ class Node:
 def initiateArrayToBinary(array):
     return arrayToBinary(array, 0, len(array) - 1)
 
-
 def arrayToBinary(array, start, end):
+
+    # I still don't undrstand this part
     if start > end:
         return ''
     mid = (start + end) // 2
     root = Node(array[mid])
+    # this respects well the definition of a binary search tree => left < node
     root.left = arrayToBinary(array, start, mid - 1)
+    # this respects well the definition of a binary search tree => left > node
     root.right = arrayToBinary(array, mid + 1, end)
+    # what happens ? In each recursive call 
     return root
 
 testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 18, 22, 43, 144, 515, 4123]
