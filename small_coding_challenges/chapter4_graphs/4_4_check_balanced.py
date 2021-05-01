6961583abf5785fbb142bdd28f536787e97a6b92
hardvester <jeskoed@gmail.com>
2021-05-01 13:18:24 +0200

    finished

--- small_coding_challenges/chapter4_graphs/4_4_check_balanced.py
+++ small_coding_challenges/chapter4_graphs/4_4_check_balanced.py
@@ -5,6 +5,29 @@ class TreeNode:
         self.right = right
 
 
+def isBalanced(node):
+    if balancedHeight(node) > -1:
+        return True
+    return False
+
+def balancedHeight(node):
+    if node is None:
+        return 0
+    h1 = balancedHeight(node.left)
+    h2 = balancedHeight(node.right)
+
+    if h1 == -1 or h2 == -1:
+        return -1
+    if abs(h1-h2) > 1:
+        return -1
+
+    if h1 > h2:
+        return h1+1
+    return h2+1   
+
+
+
+'''
 def isBalanced(node, extr, level):
     # what am I missing ? I need to keep track of the depth of the tree
     if node is not None:
@@ -30,7 +53,8 @@ def isBalancedBoolean(node):
         return False
     else:
         return True
+'''
 
 if __name__ == '__main__':
     root_node = TreeNode('A', TreeNode('B'), TreeNode('C', TreeNode('D', TreeNode('E',TreeNode('F', TreeNode('G'))))))
-    print(isBalancedBoolean(root_node))
+    print(isBalanced(root_node))
