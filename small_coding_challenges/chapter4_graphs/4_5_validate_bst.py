d42e528bba141074428a31f6cff377cec621b166
hardvester <jeskoed@gmail.com>
2021-05-02 15:04:19 +0200

    final

--- small_coding_challenges/chapter4_graphs/4_5_validate_bst.py
+++ small_coding_challenges/chapter4_graphs/4_5_validate_bst.py
@@ -7,27 +7,28 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# checking only if the previous value is smaller than the current value
+class WrapInt:
+    def __init__(self):
+        self.data = None
 
-def validate_bst(node, prev_node_data = None):
+def validate_bst(node, wrapint):
     if node == None:
         return True
-    if not validate_bst(node.left):
+    if not validate_bst(node.left, wrapint):
         return False
-    if prev_node_data != None and prev_node_data < node.data:
+    if wrapint.data != None and node.data <= wrapint.data:
         return False
-    prev_node_data = node.data
-    if not validate_bst(node.right):
+    wrapint.data = node.data
+    if not validate_bst(node.right, wrapint):
         return False
     return True
-    
-        
 
 if __name__ == '__main__':
     root_node = TreeNode(8)
     root_node.left = TreeNode(4)
     root_node.right = TreeNode(10)
-    root_node.left.left = TreeNode(2)
+    root_node.left.left = TreeNode(21)
     root_node.left.right = TreeNode(6)
     root_node.right.right = TreeNode(20)
-
-    print(validate_bst(root_node))
+    print(validate_bst(root_node, WrapInt()))
